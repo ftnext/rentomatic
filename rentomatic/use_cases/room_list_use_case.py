e01e58171df2403c338e786daa27096f762a3d50
ftnext <takuyafjp+develop@gmail.com>
2020-12-31 19:37:11 +0900

    [feat] enable use case to handle invalid request object (badly formatted case)

--- rentomatic/use_cases/room_list_use_case.py
+++ rentomatic/use_cases/room_list_use_case.py
@@ -1,3 +1,4 @@
+from rentomatic.request_objects import room_list_request_object as req
 from rentomatic.response_objects import response_objects as res
 
 
@@ -6,6 +7,10 @@ class RoomListUseCase:
         self.repo = repo
 
     def execute(self, request):
+        if isinstance(request, req.InvalidRequestObject):
+            return res.ResponseFailure.build_from_invalid_request_object(
+                request
+            )
         try:
             rooms = self.repo.list(filters=request.filters)
         except Exception as ex:
