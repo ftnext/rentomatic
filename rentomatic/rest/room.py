bb97b06443b4e93b5a7c2e5d052ddeb26c8d8fb9
ftnext <takuyafjp+develop@gmail.com>
2020-12-31 20:04:04 +0900

    [feat] make room method to create empty filter dict, create request object and handle response object

--- rentomatic/rest/room.py
+++ rentomatic/rest/room.py
@@ -3,6 +3,7 @@ import json
 from flask import Blueprint, Response
 
 from rentomatic.repository import memrepo as mr
+from rentomatic.request_objects import room_list_request_object as req
 from rentomatic.serializers import room_json_serializer as ser
 from rentomatic.use_cases import room_list_use_case as uc
 
@@ -33,12 +34,15 @@ room3 = {
 
 @blueprint.route("/rooms", methods=["GET"])
 def room():
+    query_str_params = {"filters": {}}
+    request_object = req.RoomListRequestObject.from_dict(query_str_params)
+
     repo = mr.MemRepo([room1, room2, room3])
     use_case = uc.RoomListUseCase(repo)
-    result = use_case.execute()
+    response = use_case.execute(request_object)
 
     return Response(
-        json.dumps(result, cls=ser.RoomJsonEncoder),
+        json.dumps(response.value, cls=ser.RoomJsonEncoder),
         mimetype="application/json",
         status=200,
     )
