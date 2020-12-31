d3a1631a92d0f177695b217dddf040f9a28c532c
ftnext <takuyafjp+develop@gmail.com>
2020-12-31 13:11:29 +0900

    [test] add filters attribute test code to request object

--- tests/request_objects/test_room_list_request_objects.py
+++ tests/request_objects/test_room_list_request_objects.py
@@ -4,10 +4,12 @@ from rentomatic.request_objects import room_list_request_object as req
 def test_build_room_list_request_object_without_parameters():
     request = req.RoomListRequestObject()
 
+    assert request.filters is None
     assert bool(request) is True
 
 
 def test_build_room_list_request_object_from_empty_dict():
     request = req.RoomListRequestObject.from_dict({})
 
+    assert request.filters is None
     assert bool(request) is True
