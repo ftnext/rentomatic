d4d2703e7de2f83a9884c1ca97eb26865809e2b0
ftnext <takuyafjp+develop@gmail.com>
2020-12-31 17:33:29 +0900

    [test] add test item of response success object's type and value

--- tests/response_objects/test_response_objects.py
+++ tests/response_objects/test_response_objects.py
@@ -10,3 +10,10 @@ def response_value():
 
 def test_response_success_is_true(response_value):
     assert bool(res.ResponseSuccess(response_value)) is True
+
+
+def test_response_success_has_type_and_value(response_value):
+    response = res.ResponseSuccess(response_value)
+
+    assert response.type == res.ResponseSuccess.SUCCESS
+    assert response.value == response_value
