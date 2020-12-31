7bc9e09aac4269ba187c214c2f54f9c1bd12110d
ftnext <takuyafjp+develop@gmail.com>
2020-12-31 18:23:32 +0900

    [feat] implement format message method and enable to build from Exception

--- rentomatic/response_objects/response_objects.py
+++ rentomatic/response_objects/response_objects.py
@@ -1,7 +1,12 @@
 class ResponseFailure:
     def __init__(self, type_, message):
         self.type = type_
-        self.message = message
+        self.message = self._format_message(message)
+
+    def _format_message(self, message):
+        if isinstance(message, Exception):
+            return f"{message.__class__.__name__}: {str(message)}"
+        return message
 
     def __bool__(self):
         return False
