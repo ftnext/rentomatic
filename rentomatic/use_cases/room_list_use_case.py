0e2affec408dadf2399b7669c26defd5ac538580
ftnext <takuyafjp+develop@gmail.com>
2020-12-31 19:39:42 +0900

    [refactor] update more understandable variable name

--- rentomatic/use_cases/room_list_use_case.py
+++ rentomatic/use_cases/room_list_use_case.py
@@ -6,13 +6,13 @@ class RoomListUseCase:
     def __init__(self, repo):
         self.repo = repo
 
-    def execute(self, request):
-        if isinstance(request, req.InvalidRequestObject):
+    def execute(self, request_object):
+        if isinstance(request_object, req.InvalidRequestObject):
             return res.ResponseFailure.build_from_invalid_request_object(
-                request
+                request_object
             )
         try:
-            rooms = self.repo.list(filters=request.filters)
+            rooms = self.repo.list(filters=request_object.filters)
         except Exception as ex:
             return res.ResponseFailure.build_system_error(
                 f"{ex.__class__.__name__}: {str(ex)}"
