2f319230eab6e64094c06018588e24d012ba093e
ftnext <takuyafjp+develop@gmail.com>
2020-12-31 13:00:15 +0900

    [feat] implement room list use case with request and response for extendability

--- rentomatic/use_cases/room_list_use_case.py
+++ rentomatic/use_cases/room_list_use_case.py
@@ -1,6 +1,10 @@
+from rentomatic.response_objects import response_objects as res
+
+
 class RoomListUseCase:
     def __init__(self, repo):
         self.repo = repo
 
-    def execute(self):
-        return self.repo.list()
+    def execute(self, request):
+        rooms = self.repo.list()
+        return res.ResponseSuccess(rooms)
