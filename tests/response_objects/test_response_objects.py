b0ef39399fafe82a7def03a320c6ad8b9cdf9a1c
ftnext <takuyafjp+develop@gmail.com>
2020-12-31 17:38:16 +0900

    [test] add test item that response failure is False as bool

--- tests/response_objects/test_response_objects.py
+++ tests/response_objects/test_response_objects.py
@@ -8,6 +8,16 @@ def response_value():
     return {"key": ["value1", "value2"]}
 
 
+@pytest.fixture
+def response_type():
+    return "ResponseError"
+
+
+@pytest.fixture
+def response_message():
+    return "This is a response error"
+
+
 def test_response_success_is_true(response_value):
     assert bool(res.ResponseSuccess(response_value)) is True
 
@@ -17,3 +27,7 @@ def test_response_success_has_type_and_value(response_value):
 
     assert response.type == res.ResponseSuccess.SUCCESS
     assert response.value == response_value
+
+
+def test_response_failure_is_false(response_type, response_message):
+    assert bool(res.ResponseFailure(response_type, response_message)) is False
