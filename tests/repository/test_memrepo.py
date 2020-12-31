edaa6710e676d8a002c355eb108c7edcf1a66136
ftnext <takuyafjp+develop@gmail.com>
2020-12-31 20:44:48 +0900

    [test] repository should filter with code__eq key

--- tests/repository/test_memrepo.py
+++ tests/repository/test_memrepo.py
@@ -43,3 +43,14 @@ def test_repository_list_without_parameters(room_dicts):
     rooms = [r.Room.from_dict(i) for i in room_dicts]
 
     assert repo.list() == rooms
+
+
+def test_repository_list_with_code_equal_filter(room_dicts):
+    repo = memrepo.MemRepo(room_dicts)
+
+    repo_rooms = repo.list(
+        filters={"code__eq": "fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a"}
+    )
+
+    assert len(repo_rooms) == 1
+    assert repo_rooms[0].code == "fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a"
